067e1e6a3ff6979290e4f19c716827eee678c59a
wangui.njoka <wangui.njoka@gmail.com>
2018-07-20 14:45:40 +0300

    added js but not working

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,11 @@ function rollDice(){
     var die = document.getElementById("die")
     var d1 = Math.floor(Math.random()*6)+1;
     die.innerHTML = d1;
+
+var score = document.getElementById("score")
+var scoreArr = Array.from({length:3});
+scoreArr.push(d1)
+
+
+score.innerHTML = scoreArr;
 }
